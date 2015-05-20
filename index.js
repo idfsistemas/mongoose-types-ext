e6b5a49cc96f6d540e822c6da276816ecb74dc76
Igor Rafael <igor.rafael@gmail.com>
2015-05-20 09:41:40 +0200

    feat(min-length): remove minlength, it is now a mongoose builtin

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = function(mongoose) {
-	require('./lib/string-ext/max-length.js')(mongoose);
-	require('./lib/string-ext/min-length.js')(mongoose);
 	require('./lib/string-ext/exact-length.js')(mongoose);
 	require('./lib/number-ext/exclusive-min.js')(mongoose);
 };
